9c68b785e779c549f6bc0985a5e53261af023819
viile <dakgzhu@foxmail.com>
2016-10-31 18:14:32 +0800

    modify name

--- kill.sh
+++ kill.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ps -ef | grep cas
-PID=$(ps -ef|grep bbs|awk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
+ps -ef | grep uas
+PID=$(ps -ef|grep uas|awk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
 CMD="kill -9 ${PID}"
 echo ${CMD}
 ${CMD}
-ps -ef | grep cas
+ps -ef | grep uas
