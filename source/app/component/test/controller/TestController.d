23487c067046ef7f7a88054785f1da20cef3a275
nanjia <nanjiabama@sina.com>
2018-11-23 16:34:57 +0800

    fix some issues

--- source/app/component/test/controller/TestController.d
+++ source/app/component/test/controller/TestController.d
@@ -4,6 +4,7 @@ import std.net.curl;
 import hunt.framework;
 import hunt.http.codec.http.model.Cookie;
 import hunt.logging;
+import std.digest.md;
 
 class TestController : Controller
 {
@@ -12,6 +13,7 @@ class TestController : Controller
     @Action
     string index()
     {
+        response.setHeader("content-type","text/html;charset=utf-8");
         if(request.cookie("test")){
             view.assign("valid", true);
         }
@@ -19,20 +21,39 @@ class TestController : Controller
         return view.render("test/index");
     }
 
+    string get_token()
+    {
+        string request_time = time().to!string;
+        int appid = 1000;
+        string secret = "abccefdasdf";
+        auto md5 = new MD5Digest();
+        ubyte[] hashKey = md5.digest(appid.to!string~md5.digest(request_time~secret).toHexString);
+        string sign = toHexString!(LetterCase.lower)(hashKey);
+        JSONValue res = get("http://passport.dlangchina.com/get_token?appid=1000&secret=abccefdasdf&request_time="~request_time~"&sign="~sign).parseJSON;
+        logInfo(res);
+        if(res["message"].to!string == "success"){
+            return res["data"]["token"].to!string;
+        }else{
+            return "获取token失败";
+        }
+    }
+
     @Action
     Response validate()
     {
+        // string token = 
         string service = "test";
         string callback = "192.168.32.129/postvalidate";
-        return new RedirectResponse(this.request, "http://passport.dlangforum.com/validate?service="~service~"&callback="~callback);
+        return new RedirectResponse(this.request, "http://passport.dlangchina.com/validate?service="~service~"&callback="~callback);
     }
 
     @Action
     Response postValidate()
     {
+        response.setHeader("content-type","text/html;charset=utf-8");
         string st = request.get("st");
         if(st){
-            JSONValue ret = get("http://passport.dlangforum.com/servicevalidate?st="~st).parseJSON;
+            JSONValue ret = get("http://passport.dlangchina.com/servicevalidate?st="~st).parseJSON;
             logInfo(ret);
 
             if(ret["message"].to!string == "valid"){
